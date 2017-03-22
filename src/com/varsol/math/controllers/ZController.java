17c39a3cb11102fc660e0ab45f98b818688d770f
Aleksandr Milakin <milakins@bk.ru>
2017-03-23 00:32:05 +0300

    Update ZController.java

--- src/com/varsol/math/controllers/ZController.java
+++ src/com/varsol/math/controllers/ZController.java
@@ -9,7 +9,8 @@ import com.varsol.math.models.ZNumber;
 public class ZController {
 
     public static NNumber ABS_Z_N(ZNumber number){
-        return null;
+        //	Абсолютная величина числа, результат - натуральноe 
+        return number.getNumber();
     }
 
     public static Integer POZ_Z_D(ZNumber number){
@@ -26,34 +27,43 @@ public class ZController {
     }
 
     public static ZNumber MUL_ZM_Z(ZNumber number){
-        return null;
+        //Умножение целого на (-1)
+        number.setSign(number.getSign()*(-1));
+        return number;
     }
 
     public static ZNumber TRANS_N_Z(NNumber number) {
+        //Преобразование натурального в целое
         return null;
     }
 
     public static NNumber TRANS_Z_N(ZNumber number){
-        return null;
+        //Преобразование целого неотрицательного в натуральное
+        return number.getNumber();
     }
 
     public static ZNumber ADD_ZZ_Z(ZNumber first, ZNumber second){
+        //Сложение целых чисел        
         return null;
     }
 
     public static ZNumber SUB_ZZ_Z(ZNumber first, ZNumber second){
+        //Вычитание целых чисел
         return null;
     }
 
     public static ZNumber MUL_ZZ_Z(ZNumber first, ZNumber second) {
+        //Умножение целых чисел
         return null;
     }
 
     public static NNumber DIV_ZZ_Z(ZNumber first, ZNumber second) {
+        //Частное от деления большего целого числа на меньшее или равное натуральное с остатком (делитель отличен от нуля)
         return null;
     }
 
     public static NNumber MOD_ZZ_Z(ZNumber first, ZNumber second) {
+        //Остаток от деления большего целого числа на меньшее или равное натуральное с остатком (делитель отличен от нуля)
         return null;
     }
 
